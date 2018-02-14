3b7181bd7db9707dbaad4686a5bb4116906d43be
Olga K <nimfaolya@mail.ru>
2018-02-14 09:48:51 +0100

    ready version

--- login.php
+++ login.php
@@ -1,17 +1,46 @@
 <?php
 session_start();
-$usrname = $_POST["username"];
-$password = $_POST["password"];
 
-$conn = mysqli_connect("localhost", "root", "", "DatabaseExam");
+if (isset($_POST['submit'])){
+	//db connection
+	$conn = mysqli_connect("localhost", "root", "", "databaseexam");
+	//checking for injection from user input
+	$username=mysqli_real_escape_string($conn,$_POST['username']);
+	$password=mysqli_real_escape_string($conn,$_POST['password']);
 
-$sql = "SELECT id, name FROM users WHERE username = '$usrname' AND password = '$password'";
-echo $sql . "<br>";
-$result = $conn->query($sql);
-$row = $result->fetch_assoc()
-var_dump($row);
-$id = $row["id"];
-$name = $row["name"];
-$_SESSION['userID'] = $id;
-$_SESSION["userName"] = $name;
-header("Location: index.php");//redirects back
+
+	if(empty($username) || empty($password)){
+		header("Location: index.php?login=empty");
+		exit();
+	}else{
+		$sql = "SELECT `id`, `name` FROM `users` WHERE username = '$username' AND password = '$password';";
+		$result=mysqli_query($conn,$sql);
+		$resultCheck=mysqli_num_rows($result);
+		if($result->num_rows < 1){
+			header("Location:index.php?login=error");
+			exit();
+		}else{
+			if($row=mysqli_fetch_assoc($result)){
+
+				//dehashing password and cheking
+				$hashedPswCheck =password_verify($password,$row['password']);
+				if($hashedPswCheck = false){
+					header('Location:index.php?login=password');
+					exit();
+				}elseif($hashedPswCheck = true){
+
+					//Log in the user
+					$_SESSION['userID']=$row['id'];
+					$_SESSION['userName']=$row['name'];
+					
+					header('Location:index.php?login=success');
+					exit();
+				}
+			}
+		}
+
+	}
+}else{
+	header('Location:index.php?login=error');
+	exit();
+}
